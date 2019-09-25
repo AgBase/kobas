9643340ec390eca97bb692a25ec8f698f58888b1
Amanda Cooksey <amandamcooksey@gmail.com>
2019-09-25 08:57:58 -0700

    -t not optional

--- wrapper.sh
+++ wrapper.sh
@@ -45,8 +45,8 @@ if [[ "$help" = "true" ]] ; then
 	-i INFILE can be FASTA or one-per-lineidentifiers. See -t intype for details.
 	-s SPECIES 3 or 4 letter species abbreviation (can be found here: ftp://ftp.cbi.pku.edu.cn/pub/KOBAS_3.0_DOWNLOAD/species_abbr.txt or here: https://www.kegg.jp/kegg/catalog/org_list.html)
 	-o OUTPUT file (Default is stdout.)
+	-t INTYPE (fasta:pro, fasta:nuc, blastout:xml, blastout:tab, id:ncbigi, id:uniprot, id:ensembl, id:ncbigene), default fasta:pro
     	[-l LIST available species, or list available databases for a specific species]
-	[-t INTYPE (fasta:pro, fasta:nuc, blastout:xml, blastout:tab, id:ncbigi, id:uniprot, id:ensembl, id:ncbigene), default fasta:pro]
 	[-e EVALUE expect threshold for BLAST, default 1e-5]
 	[-r RANK rank cutoff for valid hits from BLAST result, default is 5]
 	[-C COVERAGE subject coverage cutoff for BLAST, default 0]
@@ -90,37 +90,38 @@ then
     test -f sqlite3.tar && tar -xf sqlite3.tar  sqlite3/$species'.db.gz' && tar -xf sqlite3.tar sqlite3/organism.db.gz
     test -f sqlite3/$species'.db.gz' && gunzip sqlite3/$species'.db.gz'
     test -f sqlite3/organism.db.gz && gunzip sqlite3/organism.db.gz
+
     if [ -n "${coverage}" ]; then ARGS="$ARGS -C $coverage"; fi
-    if [ -n "${kobashome}" ]; then ARGS="$ARGS -k $kobashome"; fi 
+    if [ -n "${kobashome}" ]; then ARGS="$ARGS -k $kobashome"; fi
     if [ -n "${fdr}" ]; then ARGS="$ARGS -n $fdr"; fi
     if [ -n "${out}" ]; then ARGS="$ARGS -o $out"; fi
-    if [ -n "${blastp}" ]; then ARGS="$ARGS -p $blastp"; fi 
-    if [ -n "${kobasdb}" ]; then ARGS="$ARGS -q $kobasdb"; fi 
+    if [ -n "${blastp}" ]; then ARGS="$ARGS -p $blastp"; fi
+    if [ -n "${kobasdb}" ]; then ARGS="$ARGS -q $kobasdb"; fi
     if [ -n "${rank}" ]; then ARGS="$ARGS -r $rank"; fi
-    if [ -n "${blasthome}" ]; then ARGS="$ARGS -v $blasthome"; fi 
-    if [ -n "${blastx}" ]; then ARGS="$ARGS -x $blastx"; fi 
+    if [ -n "${blasthome}" ]; then ARGS="$ARGS -v $blasthome"; fi
+    if [ -n "${blastx}" ]; then ARGS="$ARGS -x $blastx"; fi
     if [ -n "${blastdb}" ]; then ARGS="$ARGS -y $blastdb"; fi
     if [ -n "${ortholog}" ]; then ARGS="$ARGS -z $ortholog"; fi
     if [ -n "${eval}" ]; then BLARGS="$BLARGS -evalue $eval"; else BLARGS="$BLARGS -evalue 1e-05"; fi
     if [ -n "${threads}" ]; then BLARGS="$BLARGS -num_threads $threads"; else BLARGS="$BLARGS -num_threads 8"; fi
-    if [[ "$intype" = "fasta:pro" ]] 
-    then 
-	test -f seq_pep.tar && tar -xf seq_pep.tar seq_pep/$species'.pep.fasta.gz'
-	test -f seq_pep/$species'.pep.fasta.gz' && gunzip seq_pep/$species'.pep.fasta.gz'
+    if [[ "$intype" = "fasta:pro" ]]
+    then
+        test -f seq_pep.tar && tar -xf seq_pep.tar seq_pep/$species'.pep.fasta.gz'
+        test -f seq_pep/$species'.pep.fasta.gz' && gunzip seq_pep/$species'.pep.fasta.gz'
         makeblastdb -in seq_pep/$species'.pep.fasta'  -parse_seqids -dbtype prot -out seq_pep/$species'.pep.fasta'
         blastp -query $infile -db seq_pep/$species'.pep.fasta' -out $species.tsv -outfmt 6 $BLARGS
-	kobas-annotate  -i $species.tsv -t blastout:tab -s $species -o $out $ARGS
+        kobas-annotate  -i $species.tsv -t blastout:tab -s $species -o $out'_annotate.txt' $ARGS
     elif [[ "$intype" = "fasta:nuc" ]]
     then
         test -f seq_pep.tar && tar -xf seq_pep.tar seq_pep/$species'.pep.fasta.gz'
-	test -f seq_pep/$species'.pep.fasta.gz' && gunzip seq_pep/$species'.pep.fasta.gz'
+        test -f seq_pep/$species'.pep.fasta.gz' && gunzip seq_pep/$species'.pep.fasta.gz'
         makeblastdb -in seq_pep/$species'.pep.fasta'  -parse_seqids -dbtype prot -out seq_pep/$species'.pep.fasta'
         blastx -query $infile -db seq_pep/$species'.pep.fasta' -out $species.tsv -outfmt 6 $BLARGS
-	kobas-annotate -i $species.tsv -t blastout:tab -s $species -o $out $ARGS
+        kobas-annotate -i $species.tsv -t blastout:tab -s $species -o $out'_annotate.txt' $ARGS
     else
-        kobas-annotate -i $infile -t $intype  -s $species -o $out  $ARGS
+        kobas-annotate -i $infile -t $intype  -s $species -o $out'_annotate.txt'  $ARGS
     fi
-    sed -ni '/\#\#/,$p' $out
+#    sed -ni '/\#\#/,$p' $out
 fi
 
 if [[ "$ident" = "true" ]]
@@ -149,6 +150,7 @@ then
     test -f sqlite3.tar && tar -xf sqlite3.tar  sqlite3/$species'.db.gz' && tar -xf sqlite3.tar sqlite3/organism.db.gz
     test -f sqlite3/$species'.db.gz' && gunzip sqlite3/$species'.db.gz'
     test -f sqlite3/organism.db.gz && gunzip sqlite3/organism.db.gz
+
     if [ -n "${coverage}" ]; then ARGS="$ARGS -C $coverage"; fi
     if [ -n "${kobashome}" ]; then ARGS="$ARGS -k $kobashome"; fi
     if [ -n "${fdr}" ]; then ARGS="$ARGS -n $fdr"; fi
